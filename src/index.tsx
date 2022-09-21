888442eaf74856ffd36767734fa4b9221ceb85ca
Mina Hanna <53411598+mhanna15@users.noreply.github.com>
2022-09-21 13:37:45 -0700

    refactored auth functions and context

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from './auth/AuthContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
