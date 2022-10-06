60e1a3a65a5550ce2d8e5c51a5733eb7fd5bdf14
warren <warrendlee1@gmail.com>
2022-10-06 03:07:24 -0700

    home page merge issues

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,38 +1,173 @@
+// import './Home.css';
+
+// import Dialog from '@mui/material/Dialog';
+// import React, { Dispatch, SetStateAction, useState } from 'react';
+
+// import Form from '../../components/Form/Form';
+// import RecruiterTable from '../../components/RecruiterTable/RecruiterTable';
+// import { RecruiterType, Template } from '../../interface';
+
+// const Home = (props: {
+//   templates: Template[];
+//   recruiters: RecruiterType[];
+//   setRecruiters: Dispatch<SetStateAction<RecruiterType[]>>;
+// }) => {
+//   const [message, setMessage] = useState<string>('');
+//   const [popUpOpen, setPopUpOpen] = useState<boolean>(false);
+
+//   return (
+//     <div className="page-root">
+//       <div className="page-content">
+//         <div className="page-header">
+//           <h1 className="page-header-title" style={{ marginBottom: '1em' }}>
+//             Dashboard
+//           </h1>
+//         </div>
+//         <button
+//           className="submit-button home-submit-button"
+//           onClick={(e) => {
+//             setPopUpOpen(true);
+//           }}
+//         >
+//           Add Recruiter
+//         </button>
+//         <RecruiterTable recruiters={props.recruiters} templates={props.templates} />
+//         {message}
+//       </div>
+//       <Dialog fullWidth={true} style={{ width: '100%' }} open={popUpOpen} onClose={() => setPopUpOpen(false)}>
+//         <Form
+//           setPopUpOpen={setPopUpOpen}
+//           setRecruiters={props.setRecruiters}
+//           setMessage={setMessage}
+//           cancel={() => setPopUpOpen(false)}
+//         />
+//       </Dialog>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
 import './Home.css';
 
 import Dialog from '@mui/material/Dialog';
-import React, { Dispatch, SetStateAction, useState } from 'react';
+import { collection, getDocs, query } from 'firebase/firestore';
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 import Form from '../../components/Form/Form';
 import RecruiterTable from '../../components/RecruiterTable/RecruiterTable';
+import { db } from '../../firebase';
 import { RecruiterType, Template } from '../../interface';
 
-const Home = (props: {
+const mockRecruiters = [
+  {
+    name: 'warren lee',
+    email: 'wlee1@gmail.com',
+    company: 'service now',
+    title: 'recruiter',
+    linkedIn: 'wlee',
+  },
+  {
+    name: 'testing',
+    email: 'hi',
+    company: 'test',
+    title: 'hey',
+    linkedIn: 'you',
+  },
+  {
+    name: 'hi1',
+    email: 'hy',
+    company: 'hi',
+    title: 'sourcer',
+    linkedIn: 'y',
+  },
+  {
+    name: 'Mina Hanna',
+    email: 'minaghanna@gmail.com',
+    company: 'Atlassian',
+    title: 'campus recruiter',
+    linkedIn: 'minaghanna',
+  },
+  {
+    name: 'Kayla Bowers',
+    email: 'kbowers@atlassian.com',
+    company: 'Atlassian',
+    title: 'recruiter',
+    linkedIn: 'linkedin.com/kaylabowers',
+  },
+];
+
+const LoggedInHome = (props: {
   templates: Template[];
   recruiters: RecruiterType[];
   setRecruiters: Dispatch<SetStateAction<RecruiterType[]>>;
 }) => {
   const [message, setMessage] = useState<string>('');
+  const [recruiters, setRecruiters] = useState<RecruiterType[]>([]);
   const [popUpOpen, setPopUpOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    const getRecruiters = async () => {
+      console.log('fetching recruiters from firebase');
+      const q = query(collection(db, 'recruiters'));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        const recruiter = doc.data();
+        setRecruiters((oldArray) => [
+          ...oldArray,
+          {
+            id: recruiter.id,
+            firstName: recruiter.firstName,
+            lastName: recruiter.lastName,
+            email: recruiter.email,
+            company: recruiter.company,
+            title: recruiter.title,
+            linkedIn: recruiter.linkedIn,
+          },
+        ]);
+      });
+    };
+    getRecruiters().catch((e) => setMessage(JSON.stringify(e)));
+    // setTimeout(() => {
+    //   setRecruiters(mockRecruiters);
+    // }, 200);
+  }, []);
 
   return (
     <div className="page-root">
       <div className="page-content">
+        <div className="page-header">
+          <h1 className="page-header-title" style={{ marginBottom: '1em' }}>
+            Dashboard
+          </h1>
+        </div>
         <button
-          className="submit-button home-submit-button"
+          className="submit-button "
           onClick={(e) => {
             setPopUpOpen(true);
           }}
         >
           Add Recruiter
         </button>
-        <RecruiterTable recruiters={props.recruiters} templates={props.templates} />
-        {message}
+        {recruiters.length === 0 ? (
+          <div className="loader" />
+        ) : (
+          <>
+            <RecruiterTable recruiters={recruiters} templates={props.templates} />
+            {message}
+          </>
+        )}
       </div>
-      <Dialog fullWidth={true} style={{ width: '100%' }} open={popUpOpen} onClose={() => setPopUpOpen(false)}>
+      <Dialog
+        sx={{ width: 1 }}
+        fullWidth={true}
+        style={{ width: '100%' }}
+        open={popUpOpen}
+        onClose={() => setPopUpOpen(false)}
+      >
         <Form
           setPopUpOpen={setPopUpOpen}
-          setRecruiters={props.setRecruiters}
+          setRecruiters={setRecruiters}
           setMessage={setMessage}
           cancel={() => setPopUpOpen(false)}
         />
@@ -41,4 +176,4 @@ const Home = (props: {
   );
 };
 
-export default Home;
+export default LoggedInHome;
