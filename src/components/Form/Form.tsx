599091374afaa402bc42b334193f2195e9c5bf5f
Mina Hanna <53411598+mhanna15@users.noreply.github.com>
2022-10-07 15:42:37 -0700

    added recruiter form validation for email and linkedin

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,7 +2,7 @@ import './Form.css';
 
 import { collection, doc, setDoc } from 'firebase/firestore';
 import _ from 'lodash';
-import React, { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
+import React, { ChangeEvent, Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 import { useAuth } from '../../auth/AuthContext';
 import { db } from '../../firebase';
@@ -20,6 +20,31 @@ const Form = (props: FormProps) => {
     props.existingRecruiter ? props.existingRecruiter : emptyRecruiter
   );
 
+  const [errors, setErrors] = useState({ email: true, linkedIn: true });
+
+  const emailIsValid = () => {
+    return (
+      String(recruiterData.email)
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gm
+        ) === null
+    );
+  };
+
+  const linkedInIsValid = () => {
+    return (
+      String(recruiterData.linkedIn)
+        .toLowerCase()
+        .match(/(?:https?:)?\/\/(?:[\w]+\.)?linkedin\.com\/in\/([\w\-\\_À-ÿ%]+)\/?/gm) === null
+    );
+  };
+
+  useEffect(() => {
+    setErrors({ email: emailIsValid(), linkedIn: linkedInIsValid() });
+    console.log(errors);
+  }, [recruiterData]);
+
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setRecruiterData((prevRecruiterData) => {
       return {
@@ -35,27 +60,43 @@ const Form = (props: FormProps) => {
 
   const handleAddNewRecruiter = async (e: any) => {
     e.preventDefault();
-    const newRecruiterRef = doc(collection(db, 'recruiters'));
-    await setDoc(newRecruiterRef, { ...recruiterData, id: newRecruiterRef.id });
-    props.setRecruiters((oldArray) => [...oldArray, { ...recruiterData, id: newRecruiterRef.id }]);
-    props.setPopUpOpen(false);
-    clearForm();
+    if (_.isEqual(errors, { email: false, linkedIn: false })) {
+      const newRecruiterRef = doc(collection(db, 'recruiters'));
+      await setDoc(newRecruiterRef, { ...recruiterData, id: newRecruiterRef.id });
+      props.setRecruiters((oldArray) => [...oldArray, { ...recruiterData, id: newRecruiterRef.id }]);
+      props.setPopUpOpen(false);
+      clearForm();
+    } else {
+      for (const [key, value] of Object.entries(errors)) {
+        if (value) {
+          alert(`${key} is invalid`);
+        }
+      }
+    }
   };
 
   const handleEditRecruiter = async (e: any) => {
     e.preventDefault();
-    if (props.existingRecruiter && currentUser?.role === 'admin') {
-      await setDoc(doc(db, 'recruiters', props.existingRecruiter.id), recruiterData);
-      props.setRecruiters((oldArray) =>
-        oldArray.map((recruiter) => {
-          if (recruiter.id === props.existingRecruiter?.id) {
-            return recruiterData;
-          }
-          return recruiter;
-        })
-      );
-      props.setPopUpOpen(false);
-      clearForm();
+    if (_.isEqual(errors, { email: false, linkedIn: false })) {
+      if (props.existingRecruiter && currentUser?.role === 'admin') {
+        await setDoc(doc(db, 'recruiters', props.existingRecruiter.id), recruiterData);
+        props.setRecruiters((oldArray) =>
+          oldArray.map((recruiter) => {
+            if (recruiter.id === props.existingRecruiter?.id) {
+              return recruiterData;
+            }
+            return recruiter;
+          })
+        );
+        props.setPopUpOpen(false);
+        clearForm();
+      }
+    } else {
+      for (const [key, value] of Object.entries(errors)) {
+        if (value) {
+          alert(`${key} is invalid`);
+        }
+      }
     }
   };
 
