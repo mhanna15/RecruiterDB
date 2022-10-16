ec26a862bed4c379d6baeaa7b54b74474ed1b2a9
Mina Hanna <53411598+mhanna15@users.noreply.github.com>
2022-10-16 00:38:56 -0700

    added analytics

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,3 +1,4 @@
+import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
@@ -16,4 +17,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore();
+export const analytics = getAnalytics(app);
 export default app;
